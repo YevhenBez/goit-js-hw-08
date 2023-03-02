32baef0a741cf1799dca8608227a842668ecf586
YevhenBez <byxgalter@bigmir.net>
2023-03-02 18:26:45 +0200

    Здесь я добавил файлы для импорта SimpleLightbox и свой код с домашки №7 по JS    для SimpleLightbox
    Здесь я добавил файлы для импорта SimpleLightbox (это строка 2 и 4 данного файла) и свой код добавил с домашки №7 по JS    для SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imageContainer = document.querySelector('.gallery');
+
+const imageMarkup = createImageCardMarkup(galleryItems);
+
+imageContainer.insertAdjacentHTML('beforeend', imageMarkup);
+
+
+function createImageCardMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}" />
+                </a>`
+    }).join('');
+}
+
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    scrollZoom: false,
+    });
