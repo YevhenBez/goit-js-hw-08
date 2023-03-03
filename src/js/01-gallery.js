e338f20bb66d5a9ed6d7f5af787ac677c758be2f
YevhenBez <byxgalter@bigmir.net>
2023-03-03 12:07:41 +0200

    Слеш я просто поставил для того, чтобы пометить данным коммитом, что я полностью сделал Задание 1 Домашки 8 по JS
    Слеш я просто поставил для того, чтобы пометить данным коммитом, что я полностью сделал Задание 1 Домашки 8 по JS. Крайняя проблема была локально. Локально живая страница не отрабатывала как надо. Для этого пришлось удалить папки    dist    и   .parcel-caсhе     и прописать в терминале VScoda    команду npm start     чтобы данные папки создались снова.

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,4 @@
-// Описаний в документації
+/// Описаний в документації
 import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
