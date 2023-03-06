18197230423218d1910dec9e27395ee9336f2956
YevhenBez <byxgalter@bigmir.net>
2023-03-06 14:36:28 +0200

    Этой командой я начал отслеживать событие timeupdate    - обновление времени воспроизведения (Задание 2 Модуля 8)
    Этой командой я начал отслеживать событие timeupdate    - обновление времени воспроизведения (согласно документации https://github.com/vimeo/player.js/#events          и  https://github.com/vimeo/player.js/#onevent-string-callback-function-void     этого плеера Vimeo) (Задание 2 Модуля 8)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,3 +2,7 @@ import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+
+player.on('timeupdate', function (currentTime) {
+    console.log(currentTime);
+});
