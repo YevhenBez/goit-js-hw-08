52abf89b12e8efc89ef5cc0f24eb13b9dbad6970
YevhenBez <115114239+YevhenBez@users.noreply.github.com>
2023-03-06 07:21:57 +0000

    Deploying to gh-pages from @ YevhenBez/goit-js-hw-08@fbae2ca3c130e9121921f2aae3e178c4c8297c4e 🚀

---
+++ 02-video.48a2b718.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("iframe");new Vimeo.Player(e);
+//# sourceMappingURL=02-video.48a2b718.js.map
---
+++ 02-video.e1de7ea5.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("iframe");new Vimeo.Player(e)}();
+//# sourceMappingURL=02-video.e1de7ea5.js.map
