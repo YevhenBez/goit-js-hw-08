c735a0b385816f546e5dcbb008c141832bc8f805
YevhenBez <byxgalter@bigmir.net>
2023-03-07 14:54:19 +0200

    Этот кусок кода с документации плеера Vimeo   позволяет после перезагрузки страницы продолжать просмотр видео с того места где был плеер до перезагрузки страницы
    Этот кусок кода с документации плеера Vimeo   позволяет после перезагрузки страницы продолжать просмотр видео с того места где был плеер до перезагрузки страницы

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,4 +7,19 @@ player.on('timeupdate', function (currentTime) {
     localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime));
 });
 
+const savedSettings = localStorage.getItem("videoplayer-current-time");
+const parsedSettings = JSON.parse(savedSettings);
 
+player.setCurrentTime(parsedSettings.seconds).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
