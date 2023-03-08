6dab68087ecdfafc39afc59046c83e79183d38d8
YevhenBez <byxgalter@bigmir.net>
2023-03-08 13:53:01 +0200

    Этим куском кода я проверял состояние Локального хранилища при перезагрузке страницы. Если там были сохранённые данные, то я ими заполнял поля формы. В противном случае поля формы остаются пустыми. Задание 3 Модуля 8.
    Этим куском кода я проверял состояние Локального хранилища при перезагрузке страницы. Если там были сохранённые данные, то я ими заполнял поля формы. В противном случае поля формы остаются пустыми. Задание 3 Модуля 8.

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,9 @@
 const form = document.querySelector('.feedback-form');
 
+const inputForm = document.querySelector('input');
+
+const textareaForm = document.querySelector('textarea');
+
 form.addEventListener('input', onFormInput);
 
 function onFormInput(event) {
@@ -17,7 +21,18 @@ function onFormInput(event) {
     // };
     localStorage.setItem("feedback-form-state", JSON.stringify(mailAndMessage)); 
 
-    console.log(mailAndMessage);
+    // console.log(mailAndMessage);
 
     // event.currentTarget.reset();
+
+
 }
+
+const savedMailAndMessage = localStorage.getItem("feedback-form-state");
+
+const parsedMailAndMessage = JSON.parse(savedMailAndMessage);
+
+if (savedMailAndMessage) {
+    inputForm.value = parsedMailAndMessage.email;
+    textareaForm.value = parsedMailAndMessage.message;
+};
