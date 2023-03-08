391e8e6d78e29f9a98d65d63745746aad25a3caa
YevhenBez <byxgalter@bigmir.net>
2023-03-08 11:05:39 +0200

    Этим куском кода я отслеживаю на форме событие    input    и сразу записываю в Локальное хранилище объект с полями    email    и    message     , в которых сохраняю текущие значения полей формы. Задание 3 Модуля 8
    Этим куском кода я отслеживаю на форме событие    input    и сразу записываю в Локальное хранилище объект с полями    email    и    message     , в которых сохраняю текущие значения полей формы. Ключ для Локального хранилища рядок "feedback-form-state"   Задание 3 Модуля 8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.feedback-form');
 
+form.addEventListener('input', onFormInput);
+
+function onFormInput(event) {
+    // event.preventDefault();
+
+    const {
+        elements: { email, message },
+    } = event.currentTarget
+
+    const mailAndMessage = { email: email.value, message: message.value };
+    
+    // if (mailAndPassword.email === "" || mailAndPassword.password === "") {
+    //     alert(message);
+    //     return;
+    // };
+    localStorage.setItem("feedback-form-state", JSON.stringify(mailAndMessage)); 
+
+    console.log(mailAndMessage);
+
+    // event.currentTarget.reset();
+}
